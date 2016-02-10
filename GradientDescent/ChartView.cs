e24bcded5c3af1b524a212923cb71d351b0cbd60
LukaszZapala <zapala.lukasz@outlook.com>
2016-02-10 10:11:21 +0100

    Add GradientDescent projects

---
+++ GradientDescent/ChartView.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace GradientDescent
+{
+    public partial class ChartView : Form
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ChartView());
+        }
+
+        private const int maxIteration = 100000;
+        private const double epsilon = 10e-6;
+
+        public ChartView()
+        {
+            InitializeComponent();
+            this.Load += LoadData;
+        }
+
+        private void LoadData(object sender, EventArgs e)
+        {
+            Gradient gradient = new Gradient(9, 0.01, 2.0, 10, 1.5);
+
+            chart1.Series["Series1"].ChartType = SeriesChartType.Spline;
+
+            gradient.SetStartPoint();
+
+            double length = 0.0;
+            int i = 0;
+            do 
+            {
+                length = gradient.GetNext();
+                chart1.Series["Series1"].Points.AddXY(i, length);
+                i++;
+            } while (length > epsilon && i < maxIteration);
+
+            Console.WriteLine("\nIteration = " + (i - 1));
+            Console.WriteLine("Distance between atoms = " + gradient.GetPoint() + "\n");
+
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            chart1.ChartAreas[0].AxisY.Minimum = 0;
+
+            chart1.Update();
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+            chart1.SaveImage("outputChar.png", ChartImageFormat.Png);
+        }
+    }
+}
---
+++ GradientDescent/Gradient.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace GradientDescent
+{
+    public class Gradient
+    {
+        private int atomCount = 0;
+        private double alpha = 0.0, A = 0.0, L = 0.0, d = 0.0;
+
+        private double[] points, derivatives;
+
+        public Gradient(int atomCount, double alpha, double A, double L, double d)
+        {
+            this.atomCount = atomCount;
+            this.alpha = alpha;
+            this.A = A; this.L = L; this.d = d;
+        }
+
+        public void SetStartPoint()
+        {
+            points = new double[atomCount];
+            for (int k = 0; k < atomCount; k++)
+            {
+                points[k] = (k * L) / (double)(atomCount - 1);
+            }
+        }
+
+        private double GetDerivatives(ref int k)
+        {
+            if (k == 0)
+            {
+                return 8 * A * (Math.Pow(points[0] - points[1], 2) - Math.Pow(d, 2)) * (points[0] - points[1]);
+            }
+            else if (k == (atomCount - 1) && atomCount != 1)
+            {
+                return 8 * A * (Math.Pow(points[atomCount - 2] - points[atomCount - 1], 2) - Math.Pow(d, 2)) 
+                    + 2 * A * (Math.Pow(points[atomCount - 1] - points[atomCount - 2], 2) - Math.Pow(d, 2));
+            }
+            else
+            {
+                return (8 * A * (Math.Pow(points[k] - points[k - 1], 2) - Math.Pow(d, 2)) * (points[k] - points[k - 1])) -
+                        (8 * A * (Math.Pow(points[k + 1] - points[k], 2) - Math.Pow(d, 2)) * (points[k + 1] - points[k]));
+            }
+        }
+
+        public double GetNext()
+        {
+            double length = 0.0;
+            derivatives = new double[atomCount];
+
+            for (int k = 0; k < atomCount; k++)
+            {
+                derivatives[k] = this.GetDerivatives(ref k);
+                length += Math.Pow(derivatives[k],2);
+                points[k] -= alpha * derivatives[k];
+            }
+
+            return Math.Sqrt(length);
+        }
+
+        public string GetPoint()
+        {
+            string tmp = "";
+            for (int i = 0; i < atomCount; i++)
+            {
+                tmp += points[i] + " ";
+                //tmp += String.Format("{0:N2}", Math.Abs(points[i] - points[i + 1])) + " ";
+            }
+            return tmp;
+        }
+    }
+}
